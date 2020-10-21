92c48aecbd631cedfe2b8a28da017dc469e14fac
Redas7 <redas.bekintis@gmail.com>
2020-10-21 14:48:07 +0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+import posts from './data.js';
+import suma from './suma.js'; 
+import atimtis from './atmintis.js';
+import sayHi from './sayHi.js';
+
+console.log(posts);
+
+console.log(suma(4,8));
+console.log (atimtis(4,8));
+
+console.log (sayHi('Rex'));
